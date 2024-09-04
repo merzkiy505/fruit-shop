60b235307f1634a64d4eb6e59d5b90f886f96a6a
Roman Kiselev <obnov2012@yandex.ru>
2024-09-04 20:49:58 +0300

    first

---
+++ README.md
@@ -0,0 +1,28 @@
+# React Shopping Cart
+
+**Shopping Cart Project** built with **React JS**, using **Context API** & **useReducer Hook**.
+
+
+## Features:
+
+- #### Add to Cart
+- #### Remove from Cart
+- #### Increment / Decrement the Quantity
+
+
+
+## Technologies used:
+
+- #### **React JS**
+- #### **SASS**
+
+
+## Available Scripts:
+
+#### `npm start`
+
+#### `npm test`
+
+#### `npm run build`
+
+#### `npm run eject`
---
+++ src/data/productsData.js
@@ -0,0 +1,68 @@
+const productsData = [
+    {
+        id: 1,
+        img: "/images/prod1.png",
+        rating: "★★★★",
+        title: "мангостин",
+        price: 54500,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "/images/prod2.png",
+        rating: "★★★★★",
+        title: "Инжир",
+        price: 31000,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: "/images/prod3.png",
+        rating: "★★★★",
+        title: "Виноград",
+        price: 84007,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "/images/prod4.png",
+        rating: "★★★★★",
+        title: "Авокадо",
+        price: 53570,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "/images/prod5.png",
+        rating: "★★★★",
+        title: "Банан",
+        price: 147990,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "/images/prod6.png",
+        rating: "★★★★★",
+        title: "Персик",
+        price: 54500,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        img: "/images/prod7.png",
+        rating: "★★★★",
+        title: "Папайя",
+        price: 24990,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        img: "/images/prod8.png",
+        rating: "★★★★",
+        title: "Арбуз",
+        price: 54500,
+        quantity: 1,
+    }
+];
+
+export default productsData;
